d7ebace72eff3db19f1a503f99a68c1327a1cbfa
AxelAngelucci <axeljesusangelucci@gmail.com>
2022-08-13 13:37:33 -0300

    Add Login and JWT

--- src/helpers/users.helpers.js
+++ src/helpers/users.helpers.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const hashPass = (password) => { //Encriptar contraseña con BcryptJS
     const salt = bcrypt.genSaltSync(10);
@@ -6,6 +8,30 @@ const hashPass = (password) => { //Encriptar contraseña con BcryptJS
     return hash;
 }
 
+const matchPass = (hash, password) => { //Comparamos la contrasena con el hash en la db
+    const match = bcrypt.compareSync(password, hash);
+    return match;
+}
+
+const generateJwt = (id, email, name)=>{ //Generamos el JWT
+    const token = jwt.sign(
+        {
+            id: id,
+            email: email,
+            name: name
+        }, process.env.JWT_SECRET,
+        {
+            expiresIn: 60 * 60 * 24 //El token tiene un tiempo de expiracion de 24hs
+        }
+    );
+    //console.log(process.env.JWT_SECRET);
+    //console.log(token);
+    return {token}
+}
+
+
 export default {
     hashPass,
+    matchPass,
+    generateJwt
 }
--- src/middlewares/users.middlewares.js
+++ src/middlewares/users.middlewares.js
@@ -1,14 +1,33 @@
 import validator from "express-validator";
+import User from "../models/User.js";
 import validResult from "./commons.js";
 
 const {check} = validator;
 
 const verifyIfIsEmail = check('email', 'Incorrect Email Format').isEmail(); //Verificar que el email sea un email
-
 const validPassLength = check('password', 'The password must be at least 5 characters long.').isLength({min: 5}); //Verificar que la contraseña tenga al menos 5 caracteres
+const validEmptyFields = check(['name', 'email', 'password' ], 'This field cant be empty').notEmpty()//Verificar que los campos no esten vacios
+const validEmptyFieldsLogin = check(['email', 'password' ], 'This field cant be empty').notEmpty()
+//Validaciones personalizadas
+const userExists = check('email').custom(
+    async (email) => {
+        const userFound = await User.findOne({email});
+        if(userFound){
+            throw new Error('Email already exists');
+        }
+    }
+); //Verificar que el email no exista en la db
+//Fin validaciones personalizadas
+
+const signupUserValidations = [verifyIfIsEmail, validPassLength, validEmptyFields, userExists, validResult];
+
+const logInUserValidations = [verifyIfIsEmail, validEmptyFieldsLogin, validResult];
+
+const validId = check('id', 'Incorrect format id').isMongoId(); //Validar que sea un id de mongo
+const getIdValidations = [validId, validResult];
 
 export {
-    verifyIfIsEmail,
-    validPassLength,
-    validResult
+    signupUserValidations,
+    logInUserValidations,
+    getIdValidations
 }
