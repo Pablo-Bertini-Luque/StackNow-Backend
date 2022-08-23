5b119824dee6c8dd765fdf83b254bff0d53279f1
Pablo-Bertini-Luque <pablobertiniluque@gmail.com>
2022-08-23 17:38:58 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import router from "./routes/index.routes.js";
 import db from "./db/config.js";
-
 
 //config
 const PORT = process.env.PORT || 4002;
@@ -13,10 +12,10 @@ const app = express();
 //middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(PORT, async ()=>{
-    await db();
-    console.log(`Server on port ${PORT}`);
+app.listen(PORT, async () => {
+  await db();
+  console.log(`Server on port ${PORT}`);
 });
