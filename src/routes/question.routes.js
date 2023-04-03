3dab5304d1085c01e2a3579c0f5ebf98f51d20d1
Pablo-Bertini-Luque <pablobertiniluque@gmail.com>
2023-04-02 21:29:10 -0300

    Agregado-ruta-searchBar

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -6,6 +6,7 @@ import {
   getQuestionId,
   getQuestionByCategory,
   questionByUser,
+  searchQuestions,
 } from "../controllers/question.controller.js";
 import { isAutenticated, validId } from "../middlewares/users.middlewares.js";
 import validResult from "../middlewares/commons.js";
@@ -19,9 +20,11 @@ const routerQuestion = Router();
 
 routerQuestion.get("/", getAllQuestion);
 
-routerQuestion.get("/:id", getQuestionId);
+routerQuestion.get("/search", searchQuestions); //Se pasa el name de la categoría
 
 routerQuestion.get("/category/:id", getQuestionByCategory); //Se pasa el id de la categoría
+
+routerQuestion.get("/:id", getQuestionId);
 
 routerQuestion.post(
   "/",
